259d4e129f738af39ef1d99b7244855e1debd7ee
hyperenju <enju@spin.mp.es.osaka-u.ac.jp>
2021-01-29 14:52:24 +0900

    動画・画像処理部をクラス化

--- processer/utils/processer.py
+++ processer/utils/processer.py
@@ -1,13 +1,19 @@
 import cv2
 import numpy as np
 
+""" 動画・画像処理をまとめたクラス
+
+    VideoProcesser: 動画の処理をオブジェクト指向で行う。メソッドチェーンが使える
+    ImageProcesser: 画像の処理。以下同文
+"""
+
 class VideoProcesser:
     """ グレイスケール前提
     例外処理などは全くやっていないので注意してください
     """
     
     def __init__(self, srcFilePath):
-        """動画の全フレームを読み込み、各種パラメータを取得する
+        """動画の全フレームをグレイスケールで読み込み、各種パラメータを取得する
 
         Args:
             srcFilePath (str): 動画ファイルのフルパス
@@ -24,8 +30,8 @@ class VideoProcesser:
             self.frames[i] = frame
 
     
-    def setBrightness(self, brightness):
-        """既存のフレームに対しtえ明るさを設定
+    def brightness(self, brightness):
+        """既存のフレームに対して明るさを設定
 
         Args:
             brightness (int): -255 ~ 255 の整数型
@@ -39,7 +45,7 @@ class VideoProcesser:
         return self
 
 
-    def setContrast(self, contrast):
+    def contrast(self, contrast):
         """既存のフレームに対してコントラストを設定
 
         Args:
@@ -49,16 +55,19 @@ class VideoProcesser:
             self: メソッドチェーンできるようにselfを返却
         """
         frames = self.frames.astype('f')
-        processed_frames = np.clip((contrast * (frames - 128) + 128), 0, 255)
+        factor = (259 * (contrast + 255)) / (255 * (259 - contrast))
+        processed_frames = np.clip(((frames - 128) * factor + 128), 0, 255)
+        # processed_frames = np.clip((contrast * (frames - 128) + 128), 0, 255)
         self.frames = processed_frames.astype('u1')
         return self
 
     
-    def removeBackground(self, averageNum):
-        """既存のフレームの0 ~ averageNumフレームの平均を取ってバックグラウンドとし、全フレームから引く
+    def subtract(self, averageNum, background_level=128):
+        """既存のフレームの0 ~ averageNum-1フレームの平均を取ってバックグラウンドとし、全フレームから引く
 
         Args:
             averageNum (int): 何フレーム目までをバックグラウンドとするかを指定。なお averageNum > self.frameNum の場合の例外などは投げていない 
+            background_level(int): 背景を何色とするか。
 
         Returns:
             self: メソッドチェーンできるようにselfを返却
@@ -67,7 +76,7 @@ class VideoProcesser:
             return self
         frames = self.frames.astype('u1')
         averageFrame = np.average(frames[:averageNum], axis=0)
-        processed_frames = np.clip((frames - averageFrame), 0, 255)
+        processed_frames = np.clip((frames - averageFrame + background_level), 0, 255)
         self.frames = processed_frames.astype('u1')
         return self
     
@@ -78,30 +87,13 @@ class VideoProcesser:
         Args:
             outFilePath (str): 書き出すファイルのフルパス
         """
-        fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
+        fourcc = cv2.VideoWriter_fourcc(*"H264")
         video = cv2.VideoWriter(outFilePath, fourcc, self.fps, (self.width, self.height), False)
         for frame in self.frames:
             video.write(frame)
         video.release()
 
 
-    @staticmethod
-    def getFirstFrame(srcFilePath):
-        """ ***静的メソッド***
-        一フレーム目を取り出すだけ。あえてクラスに属させる必要はない。
-
-        Args:
-            srcFilePath (str): 動画のファイルパス
-
-        Returns:
-            NdArray: 一フレーム目のnumpy配列
-        """
-        cap = cv2.VideoCapture(srcFilePath)
-        _, firstFrame = cap.read()
-        firstFrame = cv2.cvtColor(firstFrame, cv2.COLOR_BGR2GRAY)
-        return firstFrame
-
-    
     @staticmethod
     def getHeadFrame(srcFilePath, frameNum=5):
         """ ***静的メソッド
@@ -143,3 +135,59 @@ class VideoProcesser:
             _, frame = cap.read()
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         return frame
+
+
+class ImageProcesser:
+    """ グレイスケール前提
+    例外処理などは全くやっていないので注意してください
+    """
+
+
+    def __init__(self, srcFilePath):
+        """画像をグレイスケールで読み込んで、各種パラメータを取得する
+
+        Args:
+            srcFilePath (str): 画像ファイルのパス
+        """
+        frame = cv2.imread(srcFilePath)
+        self.frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+
+    def brightness(self, brightness):
+        """明るさを調整
+
+        Args:
+            brightness (int): 明るさの変化分
+
+        Returns:
+            self: メソッドチェーンのため自分自身を返却
+        """
+        frame = self.frame.astype('f')
+        processed_frame = np.clip((brightness + frame), 0, 255)
+        self.frame = processed_frame.astype('u1')
+        return self
+
+
+    def contrast(self, contrast):
+        """コントラストを調整
+
+        Args:
+            contrast (int): コントラストの変化分
+
+        Returns:
+            self: メソッドチェーンのため自分自身を返却
+        """
+        frame = self.frame.astype('f')
+        factor = (259 * (contrast + 255)) / (255 * (259 - contrast))
+        processed_frame = np.clip(((frame - 128) * factor + 128), 0, 255)
+        self.frame = processed_frame.astype('u1')
+        return self
+
+
+    def writeImage(self, outFilePath):
+        """画像を保存
+
+        Args:
+            outFilePath (str): 出力ファイル名
+        """
+        cv2.imwrite(outFilePath, self.frame)
